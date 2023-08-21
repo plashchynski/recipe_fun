c4e641e247a05b5b76eaaac673316418a89aee89
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-21 22:15:35 +0300

    Add bootstrap files

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
 uvicorn app.main:app --reload
 ```
+
+## References
+
+- [FastAPI](https://fastapi.tiangolo.com/)
+- [Bootstrap](https://getbootstrap.com/)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import Depends, FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import home, recipes
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(home.router)
 app.include_router(recipes.router)
