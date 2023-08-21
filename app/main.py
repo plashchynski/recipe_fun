23b7039385fbe3495aec9e2d490afc5efabf9950
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-21 22:59:48 +0300

    Connect to Postgres

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ from fastapi.staticfiles import StaticFiles
 
 from .routers import home, recipes
 
+from .db import init_db
+
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
