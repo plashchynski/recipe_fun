9bbbcccc8e0c4bb745d9937563940310440d4088
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-22 00:20:21 +0300

    Add description and directions for recipe

--- app/models.py
+++ app/models.py
@@ -5,3 +5,5 @@ from sqlmodel import SQLModel, Field
 class Recipe(SQLModel, table=True):
     id: Optional[int] = Field(default= None, nullable=False, primary_key=True)
     name: str
+    description: str
+    directions: str
