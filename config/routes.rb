8f1ac30487749c17324140d484dad0369ca9789f
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-23 13:41:26 +0300

    Add recipes controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :recipes
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!({
+    email: "admin@example.com",
+    password: "qwer1234"
+})
+
+recipes = [
+    {
+        name: "Humus",
+        description: "Hummus, also spelled hommus or houmous, is a dip of Arab origin, spread, or savory dish made from cooked, mashed chickpeas blended with tahini, lemon juice, and garlic. The standard garnish in the Middle East includes olive oil, a few whole chickpeas, parsley, and paprika."
+    },
+    {
+        name: "Chicken Kiev",
+        description: "Made from a boned and skinned breast which is cut lengthwise, pounded and stuffed with butter."
+    },
+    {
+        name: "Tagliatelle with ragù",
+        description: "type of pasta from the Emilia-Romagna and Marche regions of Italy"
+    },
+    {
+        name: "Minestrone",
+        description: "Thick soup of Italian origin made with vegetables"
+    },
+    {
+        name: "Greek salad",
+        description: "A popular salad in Greek cuisine generally made of tomatoes"
+    }
+]
+
+Recipe.create!(recipes)
