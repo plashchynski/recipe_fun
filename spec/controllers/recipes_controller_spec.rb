1f69bb7c857d524b3c35c67f512d20c400300b91
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-24 14:25:00 +0300

    Add test to check recipes#new and recipes#show templates

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe RecipesController do
   fixtures :recipes, :users
   render_views
 
+  let(:user) { users(:john) }
+  let(:recipe) { recipes(:humus) }
+
   describe "GET index" do
     it "renders the index template" do
       get :index
@@ -11,9 +14,22 @@ RSpec.describe RecipesController do
     end
 
     it "should contain the title of a test recipe" do
-      recipe = recipes(:humus)
       get :index
       expect(response.body).to include(recipe.title)
+    end
+  end
+
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET show" do
+    it "renders the new template" do
+      get :show, params: { id: recipe.id }
+      expect(response).to render_template("show")
     end
   end
 end
