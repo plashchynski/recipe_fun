aee4c8821d517bb0ed8e33e0cba4fa54766a79e0
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-24 11:51:30 +0300

    In seeds, update data if already exists

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,47 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.create!({
-    first_name: "John",
-    last_name: "Doe",
-    email: "admin@example.com",
-    password: "qwer1234"
+
+user = User.find_or_initialize_by(email: "admin@example.com")
+user.assign_attributes({
+  first_name: "John",
+  last_name: "Doe",
+  email: "admin@example.com",
+  password: "qwer1234"
 })
+user.save!
 
 recipes = [
-    {
-        name: "Humus",
-        description: "Hummus, also spelled hommus or houmous, is a dip of Arab origin, spread, or savory dish made from cooked, mashed chickpeas blended with tahini, lemon juice, and garlic. The standard garnish in the Middle East includes olive oil, a few whole chickpeas, parsley, and paprika.",
-        user: user
-    },
-    {
-        name: "Chicken Kiev",
-        description: "Made from a boned and skinned breast which is cut lengthwise, pounded and stuffed with butter.",
-        user: user
-    },
-    {
-        name: "Tagliatelle with ragù",
-        description: "type of pasta from the Emilia-Romagna and Marche regions of Italy",
-        user: user
-    },
-    {
-        name: "Minestrone",
-        description: "Thick soup of Italian origin made with vegetables",
-        user: user
-    },
-    {
-        name: "Greek salad",
-        description: "A popular salad in Greek cuisine generally made of tomatoes",
-        user: user
-    }
+  {
+    name: "Humus",
+    description: "Hummus, also spelled hommus or houmous, is a dip of Arab origin, spread, or savory dish made from cooked, mashed chickpeas blended with tahini, lemon juice, and garlic. The standard garnish in the Middle East includes olive oil, a few whole chickpeas, parsley, and paprika.",
+    user: user
+  },
+  {
+    name: "Chicken Kiev",
+    description: "Made from a boned and skinned breast which is cut lengthwise, pounded and stuffed with butter.",
+    user: user
+  },
+  {
+    name: "Tagliatelle with ragù",
+    description: "type of pasta from the Emilia-Romagna and Marche regions of Italy",
+    user: user
+  },
+  {
+    name: "Minestrone",
+    description: "Thick soup of Italian origin made with vegetables",
+    user: user
+  },
+  {
+    name: "Greek salad",
+    description: "A popular salad in Greek cuisine generally made of tomatoes",
+    user: user
+  }
 ]
 
-Recipe.create!(recipes)
+recipes.each do |recipe_data|
+  recipe = Recipe.find_or_initialize_by(name: recipe_data[:name])
+  recipe.assign_attributes(recipe_data)
+  recipe.save!
+end
+
