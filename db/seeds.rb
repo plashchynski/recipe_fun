b89fd72aaf2670b30b719bbaa6e57010364d9e09
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-24 16:39:29 +0300

    Use fixtures as seeds for development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,46 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-user = User.find_or_initialize_by(email: "admin@example.com")
-user.assign_attributes({
-  first_name: "John",
-  last_name: "Doe",
-  email: "admin@example.com",
-  password: "qwer1234"
-})
+# Load fixtures
+ENV["FIXTURES_PATH"] = "spec/fixtures"
+Rake.application["db:fixtures:load"].invoke
+
+user = User.find_by(email: "john@example.com")
+user.password = "qwer1234"
 user.save!
-
-recipes = [
-  {
-    title: "Humus",
-    description: "Hummus, also spelled hommus or houmous, is a dip of Arab origin, spread, or savory dish made from cooked, mashed chickpeas blended with tahini, lemon juice, and garlic. The standard garnish in the Middle East includes olive oil, a few whole chickpeas, parsley, and paprika.",
-    user: user
-  },
-  {
-    title: "Chicken Kiev",
-    description: "Made from a boned and skinned breast which is cut lengthwise, pounded and stuffed with butter.",
-    user: user
-  },
-  {
-    title: "Tagliatelle with ragù",
-    description: "type of pasta from the Emilia-Romagna and Marche regions of Italy",
-    user: user
-  },
-  {
-    title: "Minestrone",
-    description: "Thick soup of Italian origin made with vegetables",
-    user: user
-  },
-  {
-    title: "Greek salad",
-    description: "A popular salad in Greek cuisine generally made of tomatoes",
-    user: user
-  }
-]
-
-recipes.each do |recipe_data|
-  recipe = Recipe.find_or_initialize_by(name: recipe_data[:name])
-  recipe.assign_attributes(recipe_data)
-  recipe.save!
-end
-
