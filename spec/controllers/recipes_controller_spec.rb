a68f1b5a770bed5c5e925e64131902bd299bb174
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-24 14:22:04 +0300

    Add fixtures and test that the recipes#index actually show the recipes

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RecipesController do
+  fixtures :recipes, :users
+  render_views
+
   describe "GET index" do
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
+    end
+
+    it "should contain the title of a test recipe" do
+      recipe = recipes(:humus)
+      get :index
+      expect(response.body).to include(recipe.title)
     end
   end
 end
