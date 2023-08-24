507ed61dee933fef605214de830ff74211ef726d
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-08-24 14:52:54 +0300

    Display edit and delete button to only authors

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe RecipesController do
   let(:recipe) { recipes(:humus) }
 
   describe "GET index" do
-    it "renders the index template" do
+    it "should render the index template" do
       get :index
       expect(response).to render_template("index")
     end
@@ -20,16 +20,38 @@ RSpec.describe RecipesController do
   end
 
   describe "GET new" do
-    it "renders the new template" do
+    it "should render the new template" do
       get :new
       expect(response).to render_template("new")
     end
   end
 
   describe "GET show" do
-    it "renders the new template" do
+    it "should render the new template" do
       get :show, params: { id: recipe.id }
       expect(response).to render_template("show")
+    end
+
+    it "should not display the edit button to anonymous user" do
+      get :show, params: { id: recipe.id }
+      expect(response.body).not_to include("Edit")
+    end
+
+    it "should not display the delete button to anonymous user" do
+      get :show, params: { id: recipe.id }
+      expect(response.body).not_to include("Delete")
+    end
+
+    it "should display the edit button to the recipe author" do
+      sign_in user
+      get :show, params: { id: recipe.id }
+      expect(response.body).to include("Edit")
+    end
+
+    it "should display the delete button to the recipe author" do
+      sign_in user
+      get :show, params: { id: recipe.id }
+      expect(response.body).to include("Delete")
     end
   end
 end
